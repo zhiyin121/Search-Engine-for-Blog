68efcfd7f0ba3fad263f19393b6e65c521742900
zhiyin121 <zhiyin121@qq.com>
2021-06-02 13:26:30 +0200

    update methods

--- analysis_query.py
+++ analysis_query.py
@@ -8,7 +8,7 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 import nltk
-nltk.download('wordnet')
+# nltk.download('wordnet')
 from nltk.corpus import wordnet
 
 from negspacy.negation import Negex # negate name entity
@@ -75,56 +75,53 @@ def clean_query(query):
             cleaned_query.append(lemma)
     return cleaned_query
 
-def enrich_query(query_list):
-    synonyms_word = []
-    related_word = []
-    negation_word = []
-    antonyms_list = []; not_phrase_list = []
-    delete_set = set()
 
+def get_negation(query_list):
+    delete_set = set()
+    not_phrase_set = set()
     for index in range(len(query_list)):
         token = query_list[index]
-        # Related word
-        if not nlp(token)[0].is_stop:
-            t_synonyms_set, t_syn_definition_set = get_synonyms(token)
-            related_word += list(t_syn_definition_set)
-            synonyms_word += list(t_synonyms_set)
         # When it contains a negative expression
         ## not + adj/v -> ['not adj/v.'] or ['antonyms']
         ## not + n -> delete ['not v/n'] (not yet done)
-
         if token == 'not':
             next_token = query_list[index+1]
-            # Compose phrases containing not
-            not_phrase_list.append('not ' + next_token)
+            # Compose phrases containing not (semantically related)
+            # Bonus points if the document is included, no reduction if it is not
+            not_phrase_set.add('not ' + next_token)
             synonyms_set, syn_definition_set = get_synonyms(next_token)
             for synonym in synonyms_set:
-                not_phrase_list.append('not ' + synonym)
+                not_phrase_set.add('not ' + synonym)
             # Remove 'not' and the words it modifies
             delete_set.add('not')
             delete_set.add(next_token)
             # Get the antonym of the word modified by 'not'
             antonyms_set, ant_definition_set = get_antonyms(next_token)
-            if antonyms_set != set():
-                antonyms_list += list(antonyms_set)
-
-    negation_word = not_phrase_list + antonyms_list
-    related_word += list(ant_definition_set)
-
-    return synonyms_word, negation_word, related_word
+    return antonyms_set, not_phrase_set, delete_set
 
 
-def deal_negation(query):
-    # not + adj -> ['not adj.'] or ['antonyms']
-    return
-
-
-def get_related():
-    return
 
 def get_hyponyms():
     return
 
+# Get related enterty or properties (KG)
+def get_related():
+    return
+
+
+def enrich_query(query_list):
+    # Get the phrase containing 'not' and discover the semantically similar word
+    antonyms_set, not_phrase_set, delete_set = get_negation(query_list)
+    
+    # Get each word's synonym except stopwords
+    synonyms_set = set(); definition_set = set()
+    for index in range(len(query_list)):
+        token = query_list[index]
+        if not nlp(token)[0].is_stop:
+            synonyms, definition = get_synonyms(token)
+            synonyms_set = synonyms_set | synonyms
+            definition_set = definition_set | definition
+    return antonyms_set, not_phrase_set, delete_set, synonyms_set, definition_set
 
 
 if __name__ == '__main__':
@@ -138,11 +135,14 @@ if __name__ == '__main__':
 
     query = 'I\'m not hapyy, but he doesn\'t know.'
     cleaned_query = clean_query(query)
-    synonyms_word, negation_word, related_word = enrich_query(cleaned_query)
+    antonyms_set, not_phrase_set, delete_set, synonyms_set, definition_set = enrich_query(cleaned_query)
+
     print('cleaned_query:\n', cleaned_query, '\n',
-    'synonyms_word:\n', synonyms_word, '\n',
-    'negation_word:\n', negation_word, '\n',
-    'related_word:\n', related_word)
+    'synonyms_set:\n', synonyms_set, '\n',
+    'antonyms_set:\n', antonyms_set, '\n',
+    'definition_set:\n', definition_set, '\n',
+    'not_phrase_set:\n', not_phrase_set, '\n',
+    'delete_set:\n', delete_set)
 
     #print(get_synonyms('know'))
 

f4690eb19396f9f9130ce3302914eec81ac0d89f
zhiyin121 <zhiyin121@qq.com>
2021-06-02 16:56:47 +0200

    update methods

--- analysis_query.py
+++ analysis_query.py
@@ -14,8 +14,10 @@ from nltk.corpus import wordnet
 from negspacy.negation import Negex # negate name entity
 
 
-# Get synonyms list of a word from the corpus WordNet
+# Get synonyms set of a word from the corpus WordNet
 def get_synonyms(word):
+    if ' ' in word:
+        word = word.replace(' ', '_') # Able to handle phrases
     synonyms_set = set()
     definition_set = set()
     syns = wordnet.synsets(word)
@@ -36,8 +38,10 @@ def get_synonyms(word):
     return synonyms_set, definition_set
 
 
-# Get antonyms list of a word if available from the corpus WordNet
+# Get antonyms set of a word if available from the corpus WordNet
 def get_antonyms(word):
+    if ' ' in word:
+        word = word.replace(' ', '_') # Able to handle phrases
     antonyms_set = set()
     definition_set = set()
     syns = wordnet.synsets(word)
@@ -55,25 +59,42 @@ def get_antonyms(word):
             for word in definition:
                 if not nlp(word)[0].is_stop:
                     definition_set.add(word)
-    return antonyms_set, definition_set
+    return antonyms_set, definition_set # Could be empty set()
 
 
-def clean_query(query):
-    # Lemalization and lower case
-    cleaned_query = []
-    pos_list = []
-    doc = nlp(query)
-    for token in doc:
-        # Get the corresponding lemma and POS
-        lemma = token.lemma_.lower()
-        pos_list.append(token.pos_)
-        # Spelling correction
-        correct = spell.correction(lemma)
-        if correct:
-            cleaned_query.append(correct)
-        else:
-            cleaned_query.append(lemma)
-    return cleaned_query
+# Get hyponyms set of a word if available from the corpus WordNet
+# P.S. some of the output is somewhat old fashioned, e.g. 'phone' -> 'pay station'
+def get_hyponyms(word):
+    if ' ' in word:
+        word = word.replace(' ', '_') # Able to handle phrases
+    hyponyms_set = set()
+    syns = wordnet.synsets(word)
+    for token in syns:
+        hyps = token.hyponyms()
+        for hyp in hyps:
+            hyponyms_list = hyp.lemma_names()
+            for hyponym in hyponyms_list:
+                if '_' in hyponym:
+                    hyponym = hyponym.replace('_', ' ')
+                hyponyms_set.add(hyponym)
+    return hyponyms_set # Could be empty set()
+
+
+# Get hypernyms set of a word if available from the corpus WordNet
+def get_hypernyms(word):
+    if ' ' in word:
+        word = word.replace(' ', '_') # Able to handle phrases
+    hypernyms_set = set()
+    syns = wordnet.synsets(word)
+    for token in syns:
+        hyps = token.hypernyms()
+        for hyp in hyps:
+            hypernyms_list = hyp.lemma_names()
+            for hypernym in hypernyms_list:
+                if '_' in hypernym:
+                    hypernym = hypernym.replace('_', ' ')
+                hypernyms_set.add(hypernym)
+    return hypernyms_set # Could be empty set()
 
 
 def get_negation(query_list):
@@ -100,28 +121,46 @@ def get_negation(query_list):
     return antonyms_set, not_phrase_set, delete_set
 
 
-
-def get_hyponyms():
-    return
-
 # Get related enterty or properties (KG)
 def get_related():
     return
 
 
+def clean_query(query):
+    # Lemalization and lower case
+    cleaned_query = []
+    pos_list = []
+    doc = nlp(query)
+    for token in doc:
+        # Get the corresponding lemma and POS
+        lemma = token.lemma_.lower()
+        pos_list.append(token.pos_)
+        # Spelling correction
+        correct = spell.correction(lemma)
+        if correct:
+            cleaned_query.append(correct)
+        else:
+            cleaned_query.append(lemma)
+    return cleaned_query
+
+
 def enrich_query(query_list):
-    # Get the phrase containing 'not' and discover the semantically similar word
-    antonyms_set, not_phrase_set, delete_set = get_negation(query_list)
-    
-    # Get each word's synonym except stopwords
-    synonyms_set = set(); definition_set = set()
+    # Get each word(except stopwords)'s synonyms, hyponyms, and hypernyms 
+    synonyms_set = set(); definition_set = set(); hyponyms_set = set(); hypernyms_set = set()
     for index in range(len(query_list)):
         token = query_list[index]
         if not nlp(token)[0].is_stop:
             synonyms, definition = get_synonyms(token)
             synonyms_set = synonyms_set | synonyms
             definition_set = definition_set | definition
-    return antonyms_set, not_phrase_set, delete_set, synonyms_set, definition_set
+
+            hyponyms_set = get_hyponyms(token)
+            hypernyms_set = get_hypernyms(token)
+
+    # Get the phrase containing 'not' and discover the semantically similar word
+    antonyms_set, not_phrase_set, delete_set = get_negation(query_list)
+
+    return synonyms_set, definition_set, hyponyms_set, hypernyms_set, antonyms_set, not_phrase_set, delete_set
 
 
 if __name__ == '__main__':
@@ -133,18 +172,19 @@ if __name__ == '__main__':
     for i in data_lists[:2]:
         print(i.blog_id, i.user_id, i.gender, i.age, i.industry, i.astrology, i.date, i.post, '\n')'''
 
-    query = 'I\'m not hapyy, but he doesn\'t know.'
+    '''query = 'I\'m not hapyy, but he doesn\'t know.'
     cleaned_query = clean_query(query)
-    antonyms_set, not_phrase_set, delete_set, synonyms_set, definition_set = enrich_query(cleaned_query)
+    synonyms_set, definition_set, hyponyms_set, hypernyms_set, antonyms_set, not_phrase_set, delete_set = enrich_query(cleaned_query)
 
     print('cleaned_query:\n', cleaned_query, '\n',
     'synonyms_set:\n', synonyms_set, '\n',
-    'antonyms_set:\n', antonyms_set, '\n',
     'definition_set:\n', definition_set, '\n',
+    'hyponyms_set:\n', hyponyms_set, '\n',
+    'hypernyms_set:\n', hypernyms_set, '\n',
+    'antonyms_set:\n', antonyms_set, '\n',
     'not_phrase_set:\n', not_phrase_set, '\n',
-    'delete_set:\n', delete_set)
-
-    #print(get_synonyms('know'))
+    'delete_set:\n', delete_set)'''
+    print(get_hyponyms('emotion'))
 
     '''
     post_num = 0

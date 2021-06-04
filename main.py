f010aac230f58f0983793446bc53f7b2fd332d3f
zhiyin121 <zhiyin121@qq.com>
2021-06-04 05:16:57 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from get_data import GroupData, get_filename, clean_data, get_data
-from analysis_query import clean_query, AugmentedQuery
+from get_data import GroupData, get_filename, get_data
+from analysis_query import AugmentedQuery
+from get_simularity import Score
+from dp_similarity import SentenceTransformers
 
 import os
 import pickle
@@ -33,10 +35,38 @@ if __name__ == '__main__':
         engine.load_corpus(filepath, local_filepath, pickle_path)
 
     # input query
+    print('Query example:\n')
+    print(' #1 New York \t(Sensitive to named entities)')
+    print(' #2 I\'m not happy \t(Understand adjective phrases modified by \'not\')')
+    print(' #3 apple slow \t(Ambiguity)\n #4 apple pie \t(Ambiguity)\n')
+    print('What do you search for:')
     while True:
         query = input()
-        query_list, augment_obj = engine.process_query(query)
-        # augment_obj. + synonyms_set, definition_set, hyponyms_set, hypernyms_set, antonyms_set, not_phrase_set, delete_set
-        print(query_list, augment_obj.antonyms_set)
         # Compute simularity score
-
+        print('\nLodaing...')
+        score = Score()
+        query_score_dic, related_score_dic, all_score_dic = score.simularity_score(query)
+        top_100 = sorted(all_score_dic.items(),key=lambda item:item[1],reverse=True)[:100]  # [(blog_id, score)]
+        #print(top_100)
+        # Resort by vector similarity
+        re_score = SentenceTransformers()
+        print('Ranking...')
+        resort_dic = {}
+        with open('./group_data_objects.pickle', 'rb') as f:
+            data_lists = pickle.load(f)
+            for doc in top_100:
+                doc_id = doc[0] 
+                for i in data_lists:
+                    if i.blog_id == doc_id:
+                        document = i.post
+                        result = re_score.get_scores(query, document)
+                        resort_dic[doc_id] = result
+        #print(resort_top_100)
+        print('Soon to return results...')
+        resort_top_100 = sorted(resort_dic.items(),key=lambda item:item[1],reverse=True)[:100]  # [(blog_id, score)]
+        for doc in resort_top_100:
+            doc_id = doc[0]
+            for i in data_lists:
+                if i.blog_id == doc_id:
+                    print(i.blog_id, i.user_id, i.gender, i.age, i.industry, i.astrology, i.date, i.post, '\n')
+        

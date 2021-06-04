cb5a77c753aae8585cb6817e37e22009e30d27aa
zhiyin121 <zhiyin121@qq.com>
2021-06-04 05:18:36 +0200

    initial

---
+++ dp_similarity.py
@@ -0,0 +1,30 @@
+from sentence_transformers import SentenceTransformer, util
+
+class SentenceTransformers:
+    def __init__(self):
+        self.model = SentenceTransformer('paraphrase-MiniLM-L12-v2')
+        return
+
+    def get_scores(self, query, documents):  # query = [''], documents = ['doc1','doc2',...]
+        #Compute embedding for both lists
+        embeddings_q = self.model.encode(query, convert_to_tensor=True)
+        embeddings_ds = self.model.encode(documents, convert_to_tensor=True)
+
+        #Compute cosine-similarits
+        cosine_scores = util.pytorch_cos_sim(embeddings_q, embeddings_ds)
+
+        # Output the pairs with their score
+        #for i in range(len(documents)):
+            #print("{} \t\t {} \t\t Score: {:.4f}".format(query[0], documents[i], cosine_scores[0][i]))
+
+        return cosine_scores[0][0]
+
+
+query = ['The cat sits outside']
+documents = ['The dog plays in the garden',
+              'A woman watches TV',
+              'The new movie is so great']
+              
+score = SentenceTransformers()
+result = score.get_scores(query, documents)
+#print(type(result))
